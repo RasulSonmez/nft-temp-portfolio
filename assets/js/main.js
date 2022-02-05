ae0d2578bd918617f7f95685d341a69a98e06915
rasul sönmez <barbaros743@outlook.com>
2022-02-05 23:56:45 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+/*=== SHOW MENU ===*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*=== REMOVE MENU MOBILE ===*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=== SHOW SCROLL UP ===*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/* ACTIVE NAV LINK */
+document.addEventListener("DOMContentLoaded", function () {
+  const selector = ".nav__link";
+  const elems = Array.from(document.querySelectorAll(selector));
+  const navigation = document.querySelector(".nav__list");
+
+  function makeActive(evt) {
+    const target = evt.target;
+
+    if (!target || !target.matches(selector)) {
+      return;
+    }
+
+    elems.forEach((elem) => elem.classList.remove("active-link"));
+    evt.target.classList.add("active-link");
+  }
+
+  navigation.addEventListener("mousedown", makeActive);
+});
+
+/* QUESTIONS ACCORDION */
+const accordionItems = document.querySelectorAll(".questions__item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".questions__content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
+
+/* FEATURED SWIPER */
+
+var featuredSwiper = new Swiper(".custom-swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# NFT Portfolio Website Responsive
+
+<hr/>
+
+## NFT Portfolio Website
+
+- NFT Portfolio Website, using HTML, Sass and JavaScript.
+- Color customization.
+- Smooth scrolling in each section.
+- Developed first with the Mobile First methodology, then for desktop.
+- It contains a Header, Home, About, Colleaction,Featured Artworks, Team, FAQ and Footer.
+
+### [Live Website](https://nft-temp-one-rasulsonmez.netlify.app/)
+
+### `start`
+
+Open with live server
